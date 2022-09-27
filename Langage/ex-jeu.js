0734a784f66e8fef31ab0c68466f559b5634253e
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-09-27 14:21:44 +0200

    Correction ex

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,3 +1,23 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 const readline = require('readline'); // importe l'API readline
 
 const rl = readline.createInterface({
@@ -5,16 +25,34 @@ const rl = readline.createInterface({
   output: process.stdout, // afficher dans le terminal
 });
 
-const entierAlea = 35; // à remplacer par un vrai nombre aléatoire (Ex 1)
-
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 
 function jouer() {
-  rl.question('Quel est le nombre entier ? ', (answer) => {
-    // pour rejouer :
-    jouer();
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
 
-    // pour arrêter la partie
-    // rl.close();
+  rl.question('Quel est le nombre entier ? ', (answer) => {
+    const entierSaisi = Number.parseInt(answer, 10);
+
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
